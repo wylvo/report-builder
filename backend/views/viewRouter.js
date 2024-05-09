9f05a85b7c141d3c6bcb13ddb84a066feda3fc85
William <shakeless.notifications@gmail.com>
2024-05-09 05:17:33 -0400

    MAJOR changes

--- backend/views/viewRouter.js
+++ backend/views/viewRouter.js
@@ -1,13 +1,19 @@
 import express from "express";
 import * as views from "./viewController.js";
 import * as auth from "../auth/authController.js";
+import * as formData from "../api/v1/formData/formDataController.js";
 
 const router = express.Router();
 
-router.get("/", auth.isLoggedIn, views.getDashboard);
-router.get("/reports", auth.isLoggedIn, views.getReports);
-router.get("/users", auth.isLoggedIn, views.getusers);
-router.get("/signin", auth.isLoggedIn, views.getSignInForm);
+router.get("/", auth.protect, views.getDashboard);
+router.get(
+  "/reports",
+  auth.protect,
+  formData.synchonizeFormData,
+  views.getReports
+);
+router.get("/users", auth.protect, views.getusers);
+router.get("/signin", auth.protect, views.getSignInForm);
 router.get("/me", auth.protect, views.getAccount);
 
 export { router as viewRouter };
