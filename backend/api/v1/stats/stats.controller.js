e3e6260a74264921a72eb3a453987be63d54e1ea
wylvo <89201940+wylvo@users.noreply.github.com>
2024-07-25 12:27:18 -0400

    added stats controller to get all stats stats model

--- backend/api/v1/stats/stats.controller.js
+++ backend/api/v1/stats/stats.controller.js
@@ -2,4 +2,11 @@ import { Stats } from "./stats.model.js";
 
 import { mssql, catchAsync, GlobalError } from "../router.js";
 
-export const getAllStats = catchAsync(async (req, res, next) => {});
+export const getAllStats = catchAsync(async (req, res, next) => {
+  const [stats] = await Stats.all();
+
+  res.status(200).json({
+    status: "success",
+    data: stats,
+  });
+});
